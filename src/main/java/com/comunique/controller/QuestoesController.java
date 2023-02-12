ef3dccde7be8a839468ba5b6607be291a63f69b1
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-02-12 15:25:38 -0300

    terminando controller de questoes

--- src/main/java/com/comunique/controller/QuestoesController.java
+++ src/main/java/com/comunique/controller/QuestoesController.java
@@ -1,19 +1,31 @@
 package com.comunique.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.comunique.dto.QuestoesDTO;
 import com.comunique.model.Questoes;
+import com.comunique.service.AdminsService;
 import com.comunique.service.QuestoesService;
 
 @RestController
@@ -23,24 +35,84 @@ public class QuestoesController {
 
     @Autowired
     QuestoesService questoesService;
+    @Autowired
+    AdminsService adminsService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> findQuestionIp(@PathVariable(value = "id") UUID id) {
+    public ResponseEntity<Questoes> findQuestionIp(@PathVariable(value = "id") UUID id) {
         Optional<Questoes> questao = questoesService.getQuestao(id);
         if (!questao.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Questão não encontrada");
+            return new ResponseEntity<Questoes>(HttpStatus.NOT_FOUND);
+        } else {
+            questao.get().add(linkTo(methodOn(QuestoesController.class).findAll()).withRel("Todos as Questões"));
+            questao.get().add(linkTo(methodOn(QuestoesController.class).findQuestionLimit(10))
+                    .withRel("Questões aleatorias com tamanho de 10"));
+
         }
-        return ResponseEntity.status(HttpStatus.OK).body(questao.get());
+
+        return new ResponseEntity<Questoes>(questao.get(), HttpStatus.OK);
     }
 
     @GetMapping("/LimitRange/{limit}")
-    public ResponseEntity<Object> findQuestionLimit(@PathVariable(value = "limit") int limit) {
-        return ResponseEntity.status(HttpStatus.OK).body(questoesService.findRandomRowsLimited(limit));
+    public ResponseEntity<List<Questoes>> findQuestionLimit(@PathVariable(value = "limit") int limit) {
+        List<Questoes> questoesList = questoesService.findRandomRowsLimited(limit);
+        if (questoesList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            for (Questoes questao : questoesList) {
+                UUID id = questao.getIdQuestao();
+                questao.add(linkTo(methodOn(QuestoesController.class).findQuestionIp(id)).withSelfRel());
+            }
+            return new ResponseEntity<List<Questoes>>(questoesList, HttpStatus.OK);
+        }
     }
 
     @GetMapping("/")
-    public ResponseEntity<Object> findAll() {
-        return ResponseEntity.status(HttpStatus.OK).body(questoesService.getAllQuestoes());
+    public ResponseEntity<List<Questoes>> findAll() {
+        List<Questoes> questoesList = questoesService.getAllQuestoes();
+        if (questoesList.isEmpty()) {
+            return new ResponseEntity<List<Questoes>>(HttpStatus.NO_CONTENT);
+        } else {
+            for (Questoes questao : questoesList) {
+                UUID id = questao.getIdQuestao();
+                questao.add(linkTo(methodOn(QuestoesController.class).findQuestionIp(id)).withSelfRel());
+            }
+            return new ResponseEntity<List<Questoes>>(questoesList, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/{adminNome}/{senhaAdmin}")
+    public ResponseEntity<Questoes> cadastrarQuestion(@PathVariable(value = "adminNome") String adminNome,
+            @PathVariable(value = "senhaAdmin") String senhaAdmin, @RequestBody @Valid QuestoesDTO questoesDto) {
+        if (!adminsService.Login(adminNome, senhaAdmin)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } else {
+            Questoes questao = new Questoes();
+            BeanUtils.copyProperties(questoesDto, questao);
+            Questoes questaoPersistir = questoesService.Cadastrar(questao);
+            questaoPersistir.add(
+                    linkTo(methodOn(QuestoesController.class).findQuestionIp(questao.getIdQuestao())).withSelfRel());
+            questaoPersistir.add(linkTo(methodOn(QuestoesController.class).findAll()).withRel("Todos as Questões"));
+            questaoPersistir.add(linkTo(methodOn(QuestoesController.class).findQuestionLimit(10))
+                    .withRel("Questões aleatorias com tamanho de 10"));
+            return new ResponseEntity<Questoes>(questao, HttpStatus.OK);
+        }
+
+    }
+
+    @DeleteMapping("/{adminNome}/{senhaAdmin}/{questao}")
+    public ResponseEntity<Object> deleteQuestion(@PathVariable(value = "adminNome") String adminNome,
+            @PathVariable(value = "senhaNome") String senhaAdmin, @PathVariable(value = "questao") Questoes questao) {
+        if (!adminsService.Login(adminNome, senhaAdmin)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } else {
+            try {
+                questoesService.Deletar(questao);
+                return new ResponseEntity<>(HttpStatus.OK);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
+            }
+        }
     }
 
 }
