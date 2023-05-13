e3215ba8537543ae22a9d0f4e7e4bdad7bb25272
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-05-13 20:41:47 -0300

    resolvendo bugs no docker

--- src/test/java/com/comunique/ControllerTests/QuestoesControllerTest.java
+++ src/test/java/com/comunique/ControllerTests/QuestoesControllerTest.java
@@ -32,159 +32,146 @@ import com.comunique.service.QuestoesService;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class QuestoesControllerTest {
-    @Autowired
-    TestRestTemplate testRestTemplate;
-    @Autowired
-    InstituicoesService instituicoesService;
-    @Autowired
-    AdminsService adminsService;
-    @Autowired
-    QuestoesService questoesService;
-    private Instituicoes instituicao;
-    private Admins admin;
-    private String senhaAdmin = "1561";
-    private Questoes questao;
+        @Autowired
+        TestRestTemplate testRestTemplate;
+        @Autowired
+        InstituicoesService instituicoesService;
+        @Autowired
+        AdminsService adminsService;
+        @Autowired
+        QuestoesService questoesService;
+        private Instituicoes instituicao;
+        private Admins admin;
+        private String senhaAdmin = "1561";
+        private Questoes questao;
 
-    @Before
-    public void setUp() {
-        this.instituicao = ModelCadastrosTests.CadastarInstituicoes(instituicoesService);
-        this.admin = ModelCadastrosTests.CadastrarAdmin(instituicao, adminsService, senhaAdmin);
-        this.questao = ModelCadastrosTests.cadastrarQuestao(instituicao, questoesService);
-    }
+        @Before
+        public void setUp() {
+                this.instituicao = ModelCadastrosTests.CadastarInstituicoes(instituicoesService);
+                this.admin = ModelCadastrosTests.CadastrarAdmin(instituicao, adminsService, senhaAdmin);
+                this.questao = ModelCadastrosTests.cadastrarQuestao(instituicao, questoesService);
+        }
 
-    @After
-    public void setDown() {
-        questoesService.DeletarAllByInstituicao(instituicao);
-        adminsService.DeletarAllByInstituicao(instituicao);
-        instituicoesService.Deletar(instituicao.getIdInstituicao());
-    }
+        @After
+        public void setDown() {
+                questoesService.DeletarAllByInstituicao(instituicao);
+                adminsService.DeletarAllByInstituicao(instituicao);
+                instituicoesService.Deletar(instituicao.getIdInstituicao());
+        }
 
-    @Test
-    public void getOne() throws NoSuchMethodException {
-        String URI = Reflections.getURI(QuestoesController.class, "findQuestionIp");
-        ResponseEntity<Object> response = testRestTemplate.getForEntity(URI,
-                Object.class,
-                questao.getIdQuestao());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        System.out.println(response.getBody());
-        UUID idFalso = UUID.randomUUID();
-        ResponseEntity<Object> response2 = testRestTemplate.getForEntity(URI,
-                Object.class,
-                idFalso);
-        assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
+        @Test
+        public void getOne() throws NoSuchMethodException {
+                String URI = Reflections.getURI(QuestoesController.class, "findQuestionIp");
+                ResponseEntity<Object> response = testRestTemplate.getForEntity(URI,
+                                Object.class,
+                                questao.getIdQuestao());
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                System.out.println(response.getBody());
+                UUID idFalso = UUID.randomUUID();
+                ResponseEntity<Object> response2 = testRestTemplate.getForEntity(URI,
+                                Object.class,
+                                idFalso);
+                assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
 
-    }
+        }
 
-    @Test
-    public void getLimitRange() throws NoSuchMethodException {
-        String URI = Reflections.getURI(QuestoesController.class, "findQuestionLimit");
-        ResponseEntity<Object> response = testRestTemplate.getForEntity(URI,
-                Object.class, instituicao.getIdInstituicao(), 10);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        System.out.println(response.getBody());
-        UUID idFalso = UUID.randomUUID();
-        ResponseEntity<Object> response2 = testRestTemplate.getForEntity(URI,
-                Object.class, idFalso, 10);
-        assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
-    }
+        @Test
+        public void findAll() throws NoSuchMethodException {
+                String URI = Reflections.getURI(QuestoesController.class, "findAll");
+                ResponseEntity<Object> response = testRestTemplate.getForEntity(URI,
+                                Object.class, instituicao.getIdInstituicao());
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                System.out.println(response.getBody());
+                UUID idFalso = UUID.randomUUID();
+                ResponseEntity<Object> response2 = testRestTemplate.getForEntity(URI,
+                                Object.class, idFalso);
+                assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
+        }
 
-    @Test
-    public void findAll() throws NoSuchMethodException {
-        String URI = Reflections.getURI(QuestoesController.class, "findAll");
-        ResponseEntity<Object> response = testRestTemplate.getForEntity(URI,
-                Object.class, instituicao.getIdInstituicao());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        System.out.println(response.getBody());
-        UUID idFalso = UUID.randomUUID();
-        ResponseEntity<Object> response2 = testRestTemplate.getForEntity(URI,
-                Object.class, idFalso);
-        assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
-    }
+        @Test
+        public void CadastrarTest() throws NoSuchMethodException {
+                String URI = Reflections.getURI(QuestoesController.class, "cadastrarQuestion");
+                QuestoesDTO questao = new QuestoesDTO("asdas", "fatyghg", "fasfas", "fas", "gerge", "gerge");
+                ResponseEntity<Object> response = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.POST,
+                                new HttpEntity<>(questao),
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin);
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                System.out.println(response.getBody());
+                ResponseEntity<Object> response2 = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.POST,
+                                new HttpEntity<>(questao),
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin + "a");
+                assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
+        }
 
-    @Test
-    public void CadastrarTest() throws NoSuchMethodException {
-        String URI = Reflections.getURI(QuestoesController.class, "cadastrarQuestion");
-        QuestoesDTO questao = new QuestoesDTO("asdas", "fatyghg", "fasfas", "fas", "gerge", "gerge");
-        ResponseEntity<Object> response = testRestTemplate.exchange(
-                URI,
-                HttpMethod.POST,
-                new HttpEntity<>(questao),
-                Object.class,
-                admin.getNome(),
-                senhaAdmin);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        System.out.println(response.getBody());
-        ResponseEntity<Object> response2 = testRestTemplate.exchange(
-                URI,
-                HttpMethod.POST,
-                new HttpEntity<>(questao),
-                Object.class,
-                admin.getNome(),
-                senhaAdmin + "a");
-        assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
-    }
+        @Test
+        public void AlterarQuestap() throws NoSuchMethodException {
+                String URI = Reflections.getURI(QuestoesController.class, "updateQuestao");
+                QuestoesDTO questao2 = new QuestoesDTO("asdas", "fatyghg", "fasfas", "fas", "gerge", "gerge");
+                ResponseEntity<Object> response = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.PUT,
+                                new HttpEntity<>(questao2),
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin, questao.getIdQuestao());
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                System.out.println(response.getBody());
+                ResponseEntity<Object> response2 = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.PUT,
+                                new HttpEntity<>(questao2),
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin + "5", questao.getIdQuestao());
+                assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
+                UUID idFalso = UUID.randomUUID();
+                ResponseEntity<Object> response3 = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.PUT,
+                                new HttpEntity<>(questao2),
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin, idFalso);
+                assertEquals(HttpStatus.NOT_FOUND, response3.getStatusCode());
+        }
 
-    @Test
-    public void AlterarQuestap() throws NoSuchMethodException {
-        String URI = Reflections.getURI(QuestoesController.class, "updateQuestao");
-        QuestoesDTO questao2 = new QuestoesDTO("asdas", "fatyghg", "fasfas", "fas", "gerge", "gerge");
-        ResponseEntity<Object> response = testRestTemplate.exchange(
-                URI,
-                HttpMethod.PUT,
-                new HttpEntity<>(questao2),
-                Object.class,
-                admin.getNome(),
-                senhaAdmin, questao.getIdQuestao());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        System.out.println(response.getBody());
-        ResponseEntity<Object> response2 = testRestTemplate.exchange(
-                URI,
-                HttpMethod.PUT,
-                new HttpEntity<>(questao2),
-                Object.class,
-                admin.getNome(),
-                senhaAdmin + "5", questao.getIdQuestao());
-        assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
-        UUID idFalso = UUID.randomUUID();
-        ResponseEntity<Object> response3 = testRestTemplate.exchange(
-                URI,
-                HttpMethod.PUT,
-                new HttpEntity<>(questao2),
-                Object.class,
-                admin.getNome(),
-                senhaAdmin, idFalso);
-        assertEquals(HttpStatus.NOT_FOUND, response3.getStatusCode());
-    }
+        @Test
+        public void ExcluirQuestao() throws NoSuchMethodException {
+                String URI = Reflections.getURI(QuestoesController.class, "deleteQuestion");
+                Questoes questao2 = ModelCadastrosTests.cadastrarQuestao(instituicao, questoesService);
+                ResponseEntity<Object> response = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.DELETE,
+                                null,
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin, questao2.getIdQuestao());
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                ResponseEntity<Object> response2 = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.DELETE,
+                                null,
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin + "5", questao2.getIdQuestao());
+                assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
+                UUID idFalso = UUID.randomUUID();
+                ResponseEntity<Object> response3 = testRestTemplate.exchange(
+                                URI,
+                                HttpMethod.DELETE,
+                                null,
+                                Object.class,
+                                admin.getNome(),
+                                senhaAdmin, idFalso);
+                assertEquals(HttpStatus.NOT_FOUND, response3.getStatusCode());
 
-    @Test
-    public void ExcluirQuestao() throws NoSuchMethodException {
-        String URI = Reflections.getURI(QuestoesController.class, "deleteQuestion");
-        Questoes questao2 = ModelCadastrosTests.cadastrarQuestao(instituicao, questoesService);
-        ResponseEntity<Object> response = testRestTemplate.exchange(
-                URI,
-                HttpMethod.DELETE,
-                null,
-                Object.class,
-                admin.getNome(),
-                senhaAdmin, questao2.getIdQuestao());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        ResponseEntity<Object> response2 = testRestTemplate.exchange(
-                URI,
-                HttpMethod.DELETE,
-                null,
-                Object.class,
-                admin.getNome(),
-                senhaAdmin + "5", questao2.getIdQuestao());
-        assertEquals(HttpStatus.UNAUTHORIZED, response2.getStatusCode());
-        UUID idFalso = UUID.randomUUID();
-        ResponseEntity<Object> response3 = testRestTemplate.exchange(
-                URI,
-                HttpMethod.DELETE,
-                null,
-                Object.class,
-                admin.getNome(),
-                senhaAdmin, idFalso);
-        assertEquals(HttpStatus.NOT_FOUND, response3.getStatusCode());
-
-    }
+        }
 }
